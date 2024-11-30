a10c8e3f8352de9fd59867fd2b90bfd33d2fbf61
Bembara <nothing>
2024-11-30 16:53:55 +0100

    2nd early structure

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 from dotenv import load_dotenv
 import discord
+from discord.ext import commands
+from discord import app_commands
 
 load_dotenv()
 
-class Client(discord.Client):
+class Client(commands.Bot):
     async def on_ready(self):
         print(f"Bot: {self.user} je povezan.")
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=intents)
 
-client = Client(intents=intents)
 client.run(os.getenv('DISCORD_TOKEN'))
